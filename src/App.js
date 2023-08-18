9a5e8d5a85dc10fb5dc088bb2922520363e2a462
BalThapa <info.balthapa@gmail.com>
2023-08-18 12:02:02 +0300

    learning about bootstrap and its use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Test from "./Test";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>App will be here</h1>
+      <Test />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+//import * as Bootstrap from 'react-bootstrap'; //in order to import everything from bootstrap library
+
+const Test =()=>{
+    return <div>
+        {/* <Bootstrap.Button variant="secondary" >Example Button<Bootstrap.Button> */}
+        <Button variant="secondary" >Example Button</Button>
+    </div>
+}
+
+export default Test;
