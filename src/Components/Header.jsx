565da120d772625bfaec463d191a7bcf13e7020c
BalThapa <info.balthapa@gmail.com>
2023-08-21 22:23:42 +0300

    homepage made

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Nav from './Nav';
+import './Header.css';
 
 const Header = () => {
     return (
         <div className='header'>
+            <NavLink to='/' className='Nal'><h1>Shoppo</h1></NavLink>
             <Nav />
         </div>
     );
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from "./Components/Header";
+import "./Homepage.css";
 
 const Homepage = () => {
     return (
         <div>
             <Header />
+            <div className='container'>
+                <video autoPlay loop muted play-inlineline='true'>
+                    <source src='https://v3.cdnpk.net/videvo_files/video/premium/video0240/large_watermarked/007_raznoe_03_push_empty_cart_FPpreview.mp4'/>
+                </video>
+            </div>
             <h1>Homepage</h1>
             <p>This is Homepage</p>
         </div>
