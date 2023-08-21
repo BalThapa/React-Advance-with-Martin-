a2b9005bcd47555c3d7d978e8883733fec96d24b
BalThapa <info.balthapa@gmail.com>
2023-08-21 23:13:58 +0300

    struggling with container

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,7 +5,7 @@ import "./Homepage.css";
 const Homepage = () => {
     return (
         <div>
-            <Header />
+           <Header /> 
             <div className='container'>
                 <video autoPlay loop muted play-inlineline='true'>
                     <source src='https://v3.cdnpk.net/videvo_files/video/premium/video0240/large_watermarked/007_raznoe_03_push_empty_cart_FPpreview.mp4'/>
