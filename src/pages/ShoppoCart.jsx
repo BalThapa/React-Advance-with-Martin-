15d2a7d063324d78f3e2deba70675635ba912f1a
BalThapa <info.balthapa@gmail.com>
2023-10-02 23:26:49 +0300

    Finalized

--- src/pages/ShoppoCart.jsx
+++ src/pages/ShoppoCart.jsx
@@ -15,8 +15,13 @@ const ShoppoCart = () => {
         <Container>
             <h1>Cart</h1>
             
-            {cartItems.length === 0 && <h2>Your cart is empty!! </h2>}
-            <img src="https://cdn.dribbble.com/users/5107895/screenshots/14532312/media/a7e6c2e9333d0989e3a54c95dd8321d7.gif" alt="gif" />
+            {cartItems.length === 0 &&
+            <div>
+             <h2>Your cart is empty!!</h2> 
+                <img src="https://cdn.dribbble.com/users/5107895/screenshots/14532312/media/a7e6c2e9333d0989e3a54c95dd8321d7.gif" alt="gif" />
+            </div>
+            
+        }
             {cartItems.map((item)=>(
                 <Product 
              
